9688a739c27b7f2cb4343f96e42dccd3d5064968
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2019-11-28 23:19:13 +0700

    change model pemesanan

--- application/models/Pemesanan_model.php
+++ application/models/Pemesanan_model.php
@@ -9,9 +9,12 @@ class Pemesanan_model extends CI_Model
       $this->db->select('pem.id_pemesanan, pel.nama as nama_pelanggan, pet.nama as nama_petugas, pem.total_harga, pem.tanggal_masuk, pem.tanggal_keluar, pem.status')
         ->from('ppk_pemesanan pem')
         ->join('ppk_pelanggan pel', 'pel.id_pelanggan = pem.id_pelanggan')
-        ->join('ppk_petugas pet', 'pet.id_petugas = pem.id_petugas')
-        ->where('pet.nama', $petugas)
-        ->or_where('pet.id_petugas', 0);
+				->join('ppk_petugas pet', 'pet.id_petugas = pem.id_petugas')
+				->group_start()
+					->where('pet.nama', $petugas)
+					->or_where('pet.id_petugas', 0)
+				->group_end()
+        ->where('pet.nama', $petugas);
       return $this->db->get()->result_array();
     } else {
       //ini buat detail pemesanan nya
